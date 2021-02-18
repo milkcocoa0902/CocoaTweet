5e2a5d1712fbd14ed4c91e947a75f31ba89a8e35
keita <koron0902@gmail.com>
2021-02-18 16:03:37 +0900

    APIのエントリーにFavoriteを追加した

--- src/cocoatweet/api/api.h
+++ src/cocoatweet/api/api.h
@@ -2,6 +2,7 @@
 #define COCOATWEET_API_H_
 
 #include "cocoatweet/api/status/status.h"
+#include "cocoatweet/api/favorite/favorite.h"
 #include "cocoatweet/oauth/oauth.h"
 
 namespace CocoaTweet::API {
@@ -9,9 +10,11 @@ class API {
 public:
   API(CocoaTweet::OAuth::Key _key);
   Statuses::Status status() const;
+  Favorites::Favorite favorite() const;
 
 private:
   Statuses::Status status_;
+  Favorites::Favorite favorite_;
   std::shared_ptr<CocoaTweet::OAuth::OAuth1> oauth_;
 };
 } // namespace CocoaTweet::API
