5cebef5d9c3251007ae979cd32c35376c1336f9c
keita <koron0902@gmail.com>
2021-02-23 11:53:32 +0900

    メジャーどこな例外クラスの作成

---
+++ src/cocoatweet/exception/authenticateException.h
@@ -0,0 +1,12 @@
+#ifndef COCOATWEET_EXCEPTION_AUTHENTICATEEXCEPTION_H_
+#define COCOATWEET_EXCEPTION_AUTHENTICATEEXCEPTION_H_
+
+#include <cocoatweet/exception/exception.h>
+
+namespace CocoaTweet::Exception{
+		class AuthenticateException final: Exception{
+				using Exception::Exception;
+		};
+}
+
+#endif
---
+++ src/cocoatweet/exception/exception.h
@@ -0,0 +1,18 @@
+#ifndef COCOATWEET_EXCEPTION_EXCEPTION_H_
+#define COCOATWEET_EXCEPTION_EXCEPTION_H_
+
+#include <string>
+#include <exception>
+
+namespace CocoaTweet::Exception{
+class Exception: public std::exception{
+		public:
+				Exception(const char* _msg):msg_(std::string(_msg)){}
+				const std::string& what(){return msg_;}
+				virtual ~Exception() = default;
+		protected:
+				std::string msg_;
+};
+}
+
+#endif
