9742f20199e256350bc9078d123ba922a3c8f790
keita <koron0902@gmail.com>
2021-03-04 14:00:15 +0900

    Tweetオブジェクトに機能追加

--- src/cocoatweet/api/model/tweet.h
+++ src/cocoatweet/api/model/tweet.h
@@ -12,10 +12,21 @@ public:
       : Tweet(Tweet::parse(_responseCode, _json)) {}
   static Tweet parse(const unsigned int _responseCode, const std::string& _json);
   void id(const std::string _id);
+	void createdAt(const std::string _at);
+	void text(const std::string _text);
+	void source(const std::string _source);
   const std::string id() const;
+	const std::string createdAt() const;
+	const std::string text() const;
+	const std::string source() const;
 
 private:
   std::string id_;
+	std::string createdAt_;
+	std::string text_;
+	std::string source_;
+
+
 };
 } // namespace CocoaTweet::API::Model
 
