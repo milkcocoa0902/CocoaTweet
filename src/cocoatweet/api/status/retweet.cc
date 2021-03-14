33cb1f3e18917b1040a48e69b8a383cdaf3002f8
keita <koron0902@gmail.com>
2021-03-14 23:17:42 +0900

    RTを叩くやつ(#65)

---
+++ src/cocoatweet/api/status/retweet.cc
@@ -0,0 +1,21 @@
+#include <cocoatweet/api/status/retweet.h>
+#include <iostream>
+
+namespace CocoaTweet::API::Statuses {
+        Retweet::Retweet(){}
+
+        void Retweet::id(const std::string& _id){
+  contentType_ = "application/x-www-form-urlencoded";
+  url_         = "https://api.twitter.com/1.1/statuses/retweet/" + _id + ".json";
+        }
+
+        CocoaTweet::API::Model::Tweet Retweet::process(std::weak_ptr<CocoaTweet::OAuth::OAuth1> _oauth){
+  CocoaTweet::API::Model::Tweet tweet;
+  HttpPost::process(_oauth,
+                    [&tweet](const unsigned int _responseCode, const std::string& _rcv) {
+                      tweet = CocoaTweet::API::Model::Tweet(_responseCode, _rcv);
+                    });
+
+  return tweet;
+        }
+}
