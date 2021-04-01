1482347c485ffcc02bbf82d6d399966d270b8a7d
keita <koron0902@gmail.com>
2021-04-01 21:51:45 +0900

    dm用のエントリーポイント

---
+++ src/cocoatweet/api/directMessage/directMessage.cc
@@ -0,0 +1,15 @@
+#include <cocoatweet/api/directMessage/directMessage.h>
+#include <cocoatweet/api/directMessage/new.h>
+
+namespace CocoaTweet::API::DirectMessages{
+  DirectMessage::DirectMessage(std::shared_ptr<CocoaTweet::OAuth::OAuth1> _oauth){
+      oauth_ = _oauth;
+  }
+  
+  void DirectMessage::messageCreate(const std::string& _recipient, const std::string& _message){
+   CocoaTweet::API::DirectMessages::New dm;
+   dm.recipient(_recipient);
+   dm.message(_message);
+   dm.process(oauth_);
+  }
+}
